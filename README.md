6fddb8d663c666183b5a675cb7e66cf893e07ba9
SachiniUmayangana <sachi.umay@gmail.com>
2021-10-09 10:33:49 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+This project is Testing-App Project
+
+app run
+    setup gradle and run
+    
+app build
+    gradle build 
+    
+deployment instructions
+    build and get jar and then deploy
---
+++ src/AppInitializer.java
@@ -0,0 +1,31 @@
+/*
+ *
+ *  * -------------------------------------------------------------
+ *  *  Copyright (c) GDSE- Gansters. All rights reserved
+ *  *  Licensed under the SriLankan Information  License. See LICENSE.txt in the project root for License information
+ *  * -------------------------------------------------------------
+ *
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        primaryStage.setScene(new Scene(FXMLLoader.load(this.getClass()
+                .getResource("view/CustomerForm.fxml"))));
+        primaryStage.show();  //Ctrl + ALT+ L
+
+    }
+}
